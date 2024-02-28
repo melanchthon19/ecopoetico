b3b049a20447300ae9e50aaade09197130271be2
Francisco V. Diaz <fevarga1@uc.cl>
2024-02-28 12:53:02 -0300

    added navbar logo and changed font

--- frontend_react/src/components/Navbar.tsx
+++ frontend_react/src/components/Navbar.tsx
@@ -19,12 +19,15 @@ export default function Navbar() {
           {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
               <MenuIcon />
             </IconButton> */}
-          <Stack direction="row" alignItems="center" justifyContent="space-between" width="100%">
+          <Stack direction="row" alignItems="center" justifyContent="space-between" width="100%" height="100%">
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5, duration: 2 }}>
               <a href="/">
-                <Typography variant="h6" component="div" align="left">
-                  Eco Poético
+              <Stack direction="row" spacing={2} alignItems="center" justifyContent="flex-start">
+                <img src="/assets/logo-tp_color.png" alt="logo" width="50px" />
+                <Typography variant="h4" fontFamily="Londrina Outline" fontWeight="bold" component="div" align="left">
+                  ECOPOÉTICO
                 </Typography>
+              </Stack>
               </a>
             </motion.div>
             {currentPoemSimilars && (
