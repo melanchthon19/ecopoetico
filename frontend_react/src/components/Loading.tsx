5ef3f05f8cfb1f6a344f4451ffc8f5f60e8869f0
Francisco V. Diaz <fevarga1@uc.cl>
2024-02-28 12:07:24 -0300

    added logo to loading component

--- frontend_react/src/components/Loading.tsx
+++ frontend_react/src/components/Loading.tsx
@@ -1,10 +1,28 @@
-import { Backdrop, CircularProgress } from '@mui/material';
+import { Backdrop, CircularProgress, Stack } from '@mui/material';
+import { motion } from 'framer-motion';
 
-export default function Loading({open}: {open: boolean}) {
+export default function Loading({ open }: { open: boolean }) {
   return (
     <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
-      <CircularProgress color="inherit" />
-      Cargando...
+      <Stack spacing={2} direction="column" alignItems="end" justifyContent="end">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: [0, 1, 0],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: 'linear',
+          }}
+        >
+          <img src="/assets/logo-tp_grayscale.png" alt="logo" width="100" />
+        </motion.div>
+        <div>
+          <CircularProgress color="inherit" sx={{ mx: 2 }} />
+          Cargando...
+        </div>
+      </Stack>
     </Backdrop>
   );
 }
