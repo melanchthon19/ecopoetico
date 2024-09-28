0c773ee4cb9d162f40f82d69338c26f94e4806a2
Francisco V. Diaz <fevarga1@uc.cl>
2024-09-28 12:41:54 -0300

    made card about us page and made simple animation

---
+++ frontend_react/src/components/AboutUsCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import React from 'react';
+
+interface AboutUsCardProps {
+  name: string;
+  image: string;
+  description: string;
+}
+
+export default function AboutUsCard({ name, image, description }: AboutUsCardProps) {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card raised={true} sx={{ maxWidth: 345, mx: 'auto' }}>
+        <CardMedia
+          component="img"
+          height="120"
+          image={`${import.meta.env.VITE_BASE_URL}assets/${image}`}
+          alt={{ name } + ' Profile Photo'}
+          sx={{ mt: 3, mx: 'auto', borderRadius: '50%', height: 120, width: 120, objectFit: 'cover' }}
+        />
+        <CardContent>
+          <Typography mb={3} variant="h5" align="center" fontFamily="Merriweather" fontWeight={800}>
+            {name}
+          </Typography>
+          <Typography variant="body1" align="center">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
--- frontend_react/src/pages/AboutUs.tsx
+++ frontend_react/src/pages/AboutUs.tsx
@@ -1,4 +1,9 @@
-import { Card, CardContent, CardMedia, Container, Grid, Paper, Stack, Typography } from '@mui/material';
+import { Container, Grid, Paper, Typography } from '@mui/material';
+import AboutUsCard from '../components/AboutUsCard';
+import {motion} from 'framer-motion';
+
+const bioVargas =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque velit optio corrupti ullam adipisci, iste beatae magnam! Reiciendis, vero. Ratione similique optio eligendi veniam perspiciatis necessitatibus exercitationem rem numquam voluptates?';
 
 export default function AboutUs() {
   return (
@@ -17,68 +22,13 @@ export default function AboutUs() {
       <Typography variant="h2" py={3} fontSize="2.2rem" align="center" fontFamily="Roboto" fontWeight={800}>
         Nuestro Equipo
       </Typography>
-      <Grid container spacing={3} justifyContent="center" >
-        <Grid item xs={12} sm={6} md={4}>
-          <Card raised={true} sx={{ maxWidth: 345, mx: "auto" }}>
-            <CardMedia
-              component="img"
-              height="120"
-              image={`${import.meta.env.VITE_BASE_URL}assets/vargas.jpeg`}
-              alt="Francisco Vargas Profile Photo"
-              sx={{ mt: 3, mx: 'auto', borderRadius: '50%', height: 120, width: 120, objectFit: 'cover' }}
-            />
-            <CardContent>
-              <Typography mb={3} variant="h5" align="center" fontFamily="Merriweather" fontWeight={800}>
-                Francisco Vargas
-              </Typography>
-              <Typography variant="body1" align="center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque velit optio corrupti ullam adipisci, iste beatae magnam! Reiciendis,
-                vero. Ratione similique optio eligendi veniam perspiciatis necessitatibus exercitationem rem numquam voluptates?
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card raised={true} sx={{ maxWidth: 345, mx: "auto" }}>
-            <CardMedia
-              component="img"
-              height="120"
-              image={`${import.meta.env.VITE_BASE_URL}assets/vargas.jpeg`}
-              alt="Francisco Vargas Profile Photo"
-              sx={{ mt: 3, mx: 'auto', borderRadius: '50%', height: 120, width: 120, objectFit: 'cover' }}
-            />
-            <CardContent>
-              <Typography mb={3} variant="h5" align="center" fontFamily="Merriweather" fontWeight={800}>
-                Francisco Vargas
-              </Typography>
-              <Typography variant="body1" align="center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque velit optio corrupti ullam adipisci, iste beatae magnam! Reiciendis,
-                vero. Ratione similique optio eligendi veniam perspiciatis necessitatibus exercitationem rem numquam voluptates?
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card raised={true} sx={{ maxWidth: 345, mx: "auto" }}>
-            <CardMedia
-              component="img"
-              height="120"
-              image={`${import.meta.env.VITE_BASE_URL}assets/vargas.jpeg`}
-              alt="Francisco Vargas Profile Photo"
-              sx={{ mt: 3, mx: 'auto', borderRadius: '50%', height: 120, width: 120, objectFit: 'cover' }}
-            />
-            <CardContent>
-              <Typography mb={3} variant="h5" align="center" fontFamily="Merriweather" fontWeight={800}>
-                Francisco Vargas
-              </Typography>
-              <Typography variant="body1" align="center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque velit optio corrupti ullam adipisci, iste beatae magnam! Reiciendis,
-                vero. Ratione similique optio eligendi veniam perspiciatis necessitatibus exercitationem rem numquam voluptates?
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+      <motion.div initial={{ opacity: 0, translateY: 50 }} animate={{ opacity: 1, translateY: 0 }} transition={{ delay: 0.5, duration: 1.25 }}>
+      <Grid container spacing={3} justifyContent="center">
+        <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
+        <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
+        <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
       </Grid>
+      </motion.div>
     </Container>
   );
 }
