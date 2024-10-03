2fd4302d46bfd90fd5235c409b7de7852e869560
melanchthon19 <daniel.mm91@gmail.com>
2024-10-02 23:05:19 -0300

    bio dm y foto + bio vargas

--- frontend_react/src/pages/AboutUs.tsx
+++ frontend_react/src/pages/AboutUs.tsx
@@ -3,8 +3,11 @@ import AboutUsCard from '../components/AboutUsCard';
 import {motion} from 'framer-motion';
 
 const bioVargas =
+  'Programador, literato, músico y ciclista. Magíster en Edición (UDP), licenciado en Letras con mención en Literatura y Lingüística Hispánicas y Minor en Historia de la Música (PUC). Desde que inició la pandemia, decidí reinventarme. Actualmente soy Desarrollador Web Fullstack JavaScript y Python. Muy apasionado, también, con todo lo que sea el Desarrollo de Videojuegos y la Domótica. Siempre busco continuar aprendiendo más.';
+const bioMora = 
+  'Lingüista computacional y profesor de Lengua y Literatura. Mi interés principal es aplicar modelos y técnicas del Procesamiento del Lenguaje Natural (NLP) para fines educativos, además de proyectos más lúdicos en el Desarrollo de Videojuegos para el aprendizaje de segundas lenguas. Magíster en NLP (Universidad de Edimburgo) y licencia en Letras Hispánicas (PUC).';
+const bioEspallargas = 
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque velit optio corrupti ullam adipisci, iste beatae magnam! Reiciendis, vero. Ratione similique optio eligendi veniam perspiciatis necessitatibus exercitationem rem numquam voluptates?';
-
 export default function AboutUs() {
   return (
     <Container sx={{ my: 4 }}>
@@ -24,9 +27,9 @@ export default function AboutUs() {
       </Typography>
       <motion.div initial={{ opacity: 0, translateY: 50 }} animate={{ opacity: 1, translateY: 0 }} transition={{ delay: 0.5, duration: 1.25 }}>
       <Grid container spacing={3} justifyContent="center">
+        <AboutUsCard name="Daniel Mora" image="foto-daniel-mora.jpeg" description={bioMora} />
         <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
-        <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
-        <AboutUsCard name="Francisco Vargas" image="vargas.jpeg" description={bioVargas} />
+        <AboutUsCard name="Loreto Espallargas" image="vargas.jpeg" description={bioEspallargas} />
       </Grid>
       </motion.div>
     </Container>
